a4f7347b8651cbed57273d2f0e6e4978191e22c6
manuvinnu784 <152843839+manuvinnu784@users.noreply.github.com>
2023-12-04 13:00:37 +0530

    Update DriverContext.java

--- DriverContext.java
+++ DriverContext.java
@@ -4,4 +4,9 @@ import org.openqa.selenium.WebDriver;
 
 public class DriverContext {
     public WebDriver driver;
+
+    webdrivermanager.chromedriver().setup();
+    webdriver driver= new chromeDriver();
+    driver.manage().window().maximize();
+    driver.get(url);
 }
